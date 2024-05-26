81f3597b3c65793cf977540bf00abe71e872ecf2
shvarts.na <without@email.com>
2024-05-26 19:30:07 +0300

    Init commit

---
+++ src/main/java/com/springdataexercises/SpringDataExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.springdataexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataExercisesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springdataexercises/SpringDataExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdataexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
